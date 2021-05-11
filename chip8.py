6f3a4d9f558d7fcbb9552f38ee3b0032d384650b
P.Biswanath Patra <63574390+roshanbiswanath@users.noreply.github.com>
2021-05-11 18:38:19 +0530

    Added Sound, fixed display, removed minor bugs.
    Sound was added via winsound module, The issue of the game crashing due to bad handling of object clipping at display was fixed by changing the pixel location from the list by turning it into its modulo of 2048 so that the clipping works fine. The emulator currently plays pong fine.
    The key layout was changed.

--- chip8.py
+++ chip8.py
@@ -1,137 +1,87 @@
 import pyxel
 import random
-import time
+import winsound
+#import time
 
-pyxel.init(64, 32, fps=4000)
+pyxel.init(64, 32, caption= "Chip8 Emulator" ,fps = 300)
 
 global chip8, opcode
 
-def pause():
-    k = input()
+oldKeysDict = """chip8["keysDict"] = {
+        0x0: pyxel.btn(pyxel.KEY_KP_0),
+        0x1: pyxel.btn(pyxel.KEY_KP_1),
+        0x2: pyxel.btn(pyxel.KEY_KP_2),
+        0x3: pyxel.btn(pyxel.KEY_KP_3),
+        0x4: pyxel.btn(pyxel.KEY_KP_4),
+        0x5: pyxel.btn(pyxel.KEY_KP_5),
+        0x6: pyxel.btn(pyxel.KEY_KP_6),
+        0x7: pyxel.btn(pyxel.KEY_KP_7),
+        0x8: pyxel.btn(pyxel.KEY_KP_8),
+        0x9: pyxel.btn(pyxel.KEY_KP_9),
+        0xA: pyxel.btn(pyxel.KEY_A),
+        0xB: pyxel.btn(pyxel.KEY_B),
+        0xC: pyxel.btn(pyxel.KEY_C),
+        0xD: pyxel.btn(pyxel.KEY_D),
+        0xE: pyxel.btn(pyxel.KEY_E),
+        0xF: pyxel.btn(pyxel.KEY_F)
+    }"""
 
 chip8 = {
     "V": [0] * 16,
     "memory": [0] * 4096,
-    "I":
-    0,
-    "delayTimer":
-    0,
-    "soundTimer":
-    0,
-    "PC":
-    0x200,
-    "SP":
-    -1,
+    "I": 0,
+    "delayTimer": 0,
+    "soundTimer": 0,
+    "PC": 0x200,
+    "SP": -1,
     "stack": [0] * 16,
     "keysDict": {
-        0x0: pyxel.KEY_KP_0,
-        0x1: pyxel.KEY_KP_1,
-        0x2: pyxel.KEY_KP_2,
-        0x3: pyxel.KEY_KP_3,
-        0x4: pyxel.KEY_KP_4,
-        0x5: pyxel.KEY_KP_5,
-        0x6: pyxel.KEY_KP_6,
-        0x7: pyxel.KEY_KP_7,
-        0x8: pyxel.KEY_KP_8,
-        0x9: pyxel.KEY_KP_9,
-        0xA: pyxel.KEY_A,
-        0xB: pyxel.KEY_B,
-        0xC: pyxel.KEY_C,
-        0xD: pyxel.KEY_D,
-        0xE: pyxel.KEY_E,
-        0xF: pyxel.KEY_F,
-    },
+        0x0: pyxel.btn(pyxel.KEY_X),
+        0x1: pyxel.btn(pyxel.KEY_1),
+        0x2: pyxel.btn(pyxel.KEY_2),
+        0x3: pyxel.btn(pyxel.KEY_3),
+        0x4: pyxel.btn(pyxel.KEY_Q),
+        0x5: pyxel.btn(pyxel.KEY_W),
+        0x6: pyxel.btn(pyxel.KEY_E),
+        0x7: pyxel.btn(pyxel.KEY_A),
+        0x8: pyxel.btn(pyxel.KEY_S),
+        0x9: pyxel.btn(pyxel.KEY_D),
+        0xA: pyxel.btn(pyxel.KEY_Z),
+        0xB: pyxel.btn(pyxel.KEY_C),
+        0xC: pyxel.btn(pyxel.KEY_4),
+        0xD: pyxel.btn(pyxel.KEY_R),
+        0xE: pyxel.btn(pyxel.KEY_F),
+        0xF: pyxel.btn(pyxel.KEY_V)
+},
     "displayMemory": [0] * (64 * 32),
     "fonts": [
-        0xF0,
-        0x90,
-        0x90,
-        0x90,
-        0xF0,  #// 0
-        0x20,
-        0x60,
-        0x20,
-        0x20,
-        0x70,  #// 1
-        0xF0,
-        0x10,
-        0xF0,
-        0x80,
-        0xF0,  #// 2
-        0xF0,
-        0x10,
-        0xF0,
-        0x10,
-        0xF0,  #// 3
-        0x90,
-        0x90,
-        0xF0,
-        0x10,
-        0x10,  #// 4
-        0xF0,
-        0x80,
-        0xF0,
-        0x10,
-        0xF0,  #// 5
-        0xF0,
-        0x80,
-        0xF0,
-        0x90,
-        0xF0,  #// 6
-        0xF0,
-        0x10,
-        0x20,
-        0x40,
-        0x40,  #// 7
-        0xF0,
-        0x90,
-        0xF0,
-        0x90,
-        0xF0,  #// 8
-        0xF0,
-        0x90,
-        0xF0,
-        0x10,
-        0xF0,  #// 9
-        0xF0,
-        0x90,
-        0xF0,
-        0x90,
-        0x90,  #// A
-        0xE0,
-        0x90,
-        0xE0,
-        0x90,
-        0xE0,  #// B
-        0xF0,
-        0x80,
-        0x80,
-        0x80,
-        0xF0,  #// C
-        0xE0,
-        0x90,
-        0x90,
-        0x90,
-        0xE0,  #// D
-        0xF0,
-        0x80,
-        0xF0,
-        0x80,
-        0xF0,  #// E
-        0xF0,
-        0x80,
-        0xF0,
-        0x80,
-        0x80  #// F
+        0xF0,0x90,0x90,0x90,0xF0,  #// 0
+        0x20,0x60,0x20,0x20,0x70,  #// 1
+        0xF0,0x10,0xF0,0x80,0xF0,  #// 2
+        0xF0,0x10,0xF0,0x10,0xF0,  #// 3
+        0x90,0x90,0xF0,0x10,0x10,  #// 4
+        0xF0,0x80,0xF0,0x10,0xF0,  #// 5
+        0xF0,0x80,0xF0,0x90,0xF0,  #// 6
+        0xF0,0x10,0x20,0x40,0x40,  #// 7
+        0xF0,0x90,0xF0,0x90,0xF0,  #// 8
+        0xF0,0x90,0xF0,0x10,0xF0,  #// 9
+        0xF0,0x90,0xF0,0x90,0x90,  #// A
+        0xE0,0x90,0xE0,0x90,0xE0,  #// B
+        0xF0,0x80,0x80,0x80,0xF0,  #// C
+        0xE0,0x90,0x90,0x90,0xE0,  #// D
+        0xF0,0x80,0xF0,0x80,0xF0,  #// E
+        0xF0,0x80,0xF0,0x80,0x80  #// F
     ]
 }
 
 opcode = 0
 
+
 def getROM(memory, path):
     rom = open(path, 'rb')
     for index, val in enumerate(rom.read()):
         memory[0x200 + index] = val
+
 
 #Loading Fonts
 fontstart = 0x050
@@ -143,11 +93,12 @@ test = "test_opcode.ch8"
 getROM(chip8["memory"], pong)
 #print(chip8["memory"])
 
+
 def fetch():
-    opcode = chip8["memory"][chip8["PC"]] << 8 | chip8["memory"][chip8["PC"] +
-                                                                 1]
+    opcode = chip8["memory"][chip8["PC"]] << 8 | chip8["memory"][chip8["PC"]+1]
     chip8["PC"] += 2
     return opcode
+
 
 def decode(opcode):
     #print(opcode)
@@ -233,10 +184,11 @@ def decode(opcode):
         elif arg_xxnn == 0x65:
             return "LD Vx, [I]"
 
+
 def setPixel(x, y):
     cols = 64
     rows = 32
-    print(x, y)
+    #print(x, y)
     if x > cols:
         x -= cols
     elif x < 0:
@@ -245,13 +197,14 @@ def setPixel(x, y):
         y -= rows
     elif y < 0:
         y += rows
-    pixelIndex = x + y * cols
-    print(pixelIndex)
+    pixelIndex = (x + y * cols)%2048
+    #print(pixelIndex)
     chip8["displayMemory"][pixelIndex] ^= 1
     return not chip8["displayMemory"][pixelIndex]
 
+
 def execute(s):
-    #print("Did ", s)
+    print("Instruction Executing : ", s)
     if s == "CLS":
         chip8["displayMemory"] = [0] * (64 * 32)
     elif s == "RET":
@@ -326,9 +279,9 @@ def execute(s):
         Vx = arg_x
         Vy = arg_y
         chip8["V"][15] = chip8["V"][Vx] & 0x1
-        print(chip8["V"][Vx])
+        #print(chip8["V"][Vx])
         chip8["V"][Vx] = (chip8["V"][Vx] >> 1) % 255
-        print(chip8["V"][Vx])
+        #print(chip8["V"][Vx])
         """
         val = chip8["V"][Vx]
         lsb = val & 0x1
@@ -340,7 +293,7 @@ def execute(s):
             chip8["V"][15] = 1
         else:
             chip8["V"][15] = 0
-        print(chip8["V"][Vx])
+        #print(chip8["V"][Vx])
         chip8["V"][Vx] = chip8["V"][Vx] >> 1
         """
     elif s == "SUBN Vx, Vy":
@@ -356,9 +309,9 @@ def execute(s):
         Vx = arg_x
         Vy = arg_y
         chip8["V"][15] = chip8["V"][Vx] & 0x80
-        print(chip8["V"][Vx])
+        #print(chip8["V"][Vx])
         chip8["V"][Vx] = chip8["V"][Vx] << 1
-        print(chip8["V"][Vx])
+        #print(chip8["V"][Vx])
         """
         Vx = arg_x
         if chip8["V"][Vx] & 0x80 == 1:
@@ -451,51 +404,48 @@ def execute(s):
             chip8["V"][i] = chip8["memory"][loc]
             loc += 1
 
-    #print(chip8)
-    #pause()
+
 def cpucycle():
     k = fetch()
     m = decode(k)
     execute(m)
-    time.sleep(1 / 60)
+    #time.sleep(1 / 60)
     if chip8["delayTimer"] > 0:
         chip8["delayTimer"] -= 1
-        """
     if chip8["soundTimer"] > 0:
         if chip8["soundTimer"] == 1:
-            pyxel.sound(0)
-        chip8["delayTimer"] -= 1"""
+            winsound.PlaySound("sound.wav",winsound.SND_FILENAME | winsound.SND_ASYNC)
+            #winsound.Beep(440, 10, winsound.SND_ASYNC)
+        chip8["soundTimer"] -= 1
 
 
 def update():
     running = True
-    """while chip8["delayTimer"]:
-        time.sleep(1/60)
-        chip8["delayTimer"] -= 1"""
     chip8["keysDict"] = {
-        0x0: pyxel.btn(pyxel.KEY_KP_0),
-        0x1: pyxel.btn(pyxel.KEY_KP_1),
-        0x2: pyxel.btn(pyxel.KEY_KP_2),
-        0x3: pyxel.btn(pyxel.KEY_KP_3),
-        0x4: pyxel.btn(pyxel.KEY_KP_4),
-        0x5: pyxel.btn(pyxel.KEY_KP_5),
-        0x6: pyxel.btn(pyxel.KEY_KP_6),
-        0x7: pyxel.btn(pyxel.KEY_KP_7),
-        0x8: pyxel.btn(pyxel.KEY_KP_8),
-        0x9: pyxel.btn(pyxel.KEY_KP_9),
-        0xA: pyxel.btn(pyxel.KEY_A),
-        0xB: pyxel.btn(pyxel.KEY_B),
-        0xC: pyxel.btn(pyxel.KEY_C),
-        0xD: pyxel.btn(pyxel.KEY_D),
-        0xE: pyxel.btn(pyxel.KEY_E),
-        0xF: pyxel.btn(pyxel.KEY_F)
-    }
+        0x0: pyxel.btn(pyxel.KEY_X),
+        0x1: pyxel.btn(pyxel.KEY_1),
+        0x2: pyxel.btn(pyxel.KEY_2),
+        0x3: pyxel.btn(pyxel.KEY_3),
+        0x4: pyxel.btn(pyxel.KEY_Q),
+        0x5: pyxel.btn(pyxel.KEY_W),
+        0x6: pyxel.btn(pyxel.KEY_E),
+        0x7: pyxel.btn(pyxel.KEY_A),
+        0x8: pyxel.btn(pyxel.KEY_S),
+        0x9: pyxel.btn(pyxel.KEY_D),
+        0xA: pyxel.btn(pyxel.KEY_Z),
+        0xB: pyxel.btn(pyxel.KEY_C),
+        0xC: pyxel.btn(pyxel.KEY_4),
+        0xD: pyxel.btn(pyxel.KEY_R),
+        0xE: pyxel.btn(pyxel.KEY_F),
+        0xF: pyxel.btn(pyxel.KEY_V)
+}
     #print(chip8["keysDict"])
     cpucycle()
-    print(pyxel.btnp(pyxel.KEY_Q))
-    if pyxel.btn(pyxel.KEY_Q) and running:
-        print(pyxel.btnp(pyxel.KEY_Q))
+    #print(pyxel.btnp(pyxel.KEY_Q))
+    if pyxel.btn(pyxel.KEY_ESCAPE) and running:
+        #print(pyxel.btnp(pyxel.KEY_Q))
         pyxel.quit()
+
 
 def draw():
     #pyxel.cls(0)
@@ -511,5 +461,6 @@ def draw():
         pyxel.pset(x, y, z)
     #pyxel.pset(10,10,11)
     #pyxel.rect(10, 10, 20, 20, 11)
+
 
 pyxel.run(update, draw)
